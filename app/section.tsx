188ecfdd883f9b4a946a5085aed0a439711dc9ed
keimindev <keimindev@gmail.com>
2024-10-28 15:23:32 +0900

    add icons & change category UI

--- app/section.tsx
+++ app/section.tsx
@@ -1,18 +1,24 @@
 import { useGlobalContext } from "@/context/GlobalProvider";
-import { Link, router } from "expo-router";
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { View, Text, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 
 const Section = () => {
   const list = [
-    { id: 1, content: "📚 Record your reading time", category: "Reading" },
-    { id: 2, content: "📝 Record your study time", category: "Study" },
-    { id: 3, content: "🏋🏻 Record your workout time", category: "Workout" },
-    { id: 4, content: "👩🏻‍🍳 Record your cooking time", category: "Cook" },
-    { id: 5, content: "🎨 Record your drawing time", category: "Draw" },
+    { id: 1, name: "library-books", category: "reading" },
+    { id: 2, name: "edit-note", category: "study" },
+    { id: 3, name: "directions-run", category: "workout" },
+    { id: 4, name: "draw", category: "drawing" },
+    { id: 5, name: "videogame-asset", category: "game" },
   ];
 
   const { setSection } = useGlobalContext();
+
+  const capitalize = (ch: string) => {
+    return ch.charAt(0).toUpperCase() + ch.slice(1);
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -20,23 +26,25 @@ const Section = () => {
         <Text className="text-lg font-Rsemibold text-secondary text-center m-5">
           Let&apos;s record your day!
         </Text>
-        {list.map((item) => {
-          return (
-            <View
-              key={`key-${item.id}`}
-              className="text-center bg-secondary m-3 items-center justify-center p-3 rounded-lg"
-            >
-              <Text
-                onPress={() => {
-                  router.push("/stopwatch");
-                  setSection(item.category);
-                }}
-              >
-                {item.content}
-              </Text>
-            </View>
-          );
-        })}
+        <View className="flex flex-wrap flex-row justify-center items-center mt-5">
+          {list.map((item) => {
+            return (
+              <View key={`key-${item.id}`} className="w-[130px] h-[130px] m-3 bg-secondary rounded-full text-center">
+                <TouchableOpacity
+                  onPress={() => {
+                    router.push("/stopwatch");
+                    setSection(item.category);
+                  }}
+                  activeOpacity={0.7}
+                  className="flex justify-center items-center text-center h-[130px]"
+                >
+                  <MaterialIcons name={item.name as any} size={55} color="black" />
+                  <Text className="text-sm font-Rsemibold">{capitalize(item.category)}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </View>
       </View>
     </SafeAreaView>
   );
